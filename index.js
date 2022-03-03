9096df0127bc5b6f4230ac1479f0de206ffb1352
EzequielJes <88411304+EzequielJes@users.noreply.github.com>
2022-03-02 22:03:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,155 @@
-const { WAConnection, MessageType } = require('@adiwajshing/baileys')const fs = require('fs') const prefix = '.' const iniciar = async(auth) => { const client = new WAConnection client.logger.level = 'warn'	client.version = [2, 2143, 3]		client.on('qr', () => console.log('Escanee el codigo qr'))		fs.existsSync(auth) && client.loadAuthInfo(auth)	client.on('connecting', () => console.log('Conectando...'))		client.on('open', () => console.log('Conectado exitosamente'))		await client.connect({timeoutMs: 30 * 1000})	fs.writeFileSync(auth, JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))		client.on('chat-update', (mek) => {		try { if (!mek.hasNewMessage) return if (!mek.messages) return if (mek.key && mek.key.remoteJid == 'status@broadcast') return mek = mek.messages.all()[0] if (!mek.message) return mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message const type = Object.keys(mek.message)[0] const content = JSON.stringify(mek.message) const from = mek.key.remoteJid const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : [] const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : '' const { text, extendedText, contact, listMessage, buttonsMessage, location, image, video, sticker, document, audio } = MessageType if (prefix != '') { if (!body.startsWith(prefix)) { cmd = false comm = '' } else { cmd = true comm = body.slice(1).trim().split(' ').shift().toLowerCase() } } else { cmd = false comm = body.trim().split(' ').shift().toLowerCase() } const command = comm const args = body.trim().split(/ +/).slice(1) const isCmd = body.startsWith(prefix) const q = args.join(' ') const soyYo = client.user.jid const botNumber = client.user.jid.split('@')[0] const ownerNumber = ['595994230885'] const isGroup = from.endsWith('@g.us') const sender = mek.key.fromMe ? vex.user.jid : isGroup ? mek.participant : mek.key.remoteJid const senderNumber = sender.split('@')[0] const conts = mek.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') } const pushname = mek.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-' const isMe = botNumber.includes(senderNumber) const isOwner = ownerNumber.includes(senderNumber) switch (command) {
+const { WAConnection, MessageType } = require('@adiwajshing/baileys')
+
+const fs = require('fs')
+
+const prefix = '.'
+
+const iniciar = async(auth) => {
+
+        const client = new WAConnection
+
+        
+
+        client.logger.level = 'warn'
+
+	client.version = [2, 2143, 3]	
+
+	cient.on('qr', () => console.log('Escanee el codigo qr'))
+
+	
+
+	fs.existsSync(auth) && client.loadAuthInfo(auth)
+
+	client.on('connecting', () => console.log('Conectando...'))
+
+	
+
+	client.on('open', () => console.log('Conectado exitosamente'))
+
+	
+
+	await client.connect({timeoutMs: 30 * 1000})
+
+	fs.writeFileSync(auth, JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+
+	
+
+	client.on('chat-update', (mek) => {
+
+		try {
+
+                        if (!mek.hasNewMessage) return
+
+                        if (!mek.messages) return
+
+                        if (mek.key && mek.key.remoteJid == 'status@broadcast') return
+
+                        
+
+                        mek = mek.messages.all()[0]
+
+                        if (!mek.message) return
+
+                        mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+
+                        const type = Object.keys(mek.message)[0]
+
+                        const content = JSON.stringify(mek.message)
+
+                        const from = mek.key.remoteJid
+
+                        const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+
+                        const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+
+                        const { text, extendedText, contact, listMessage, buttonsMessage, location, image, video, sticker, document, audio } = MessageType
+
+                        
+
+                        if (prefix != '') {
+
+                                if (!body.startsWith(prefix)) {
+
+                                        cmd = false
+
+                                        comm = ''
+
+                                } else {
+
+                                        cmd = true
+
+                                        comm = body.slice(1).trim().split(' ').shift().toLowerCase()
+
+                                }
+
+                        } else {
+
+                                cmd = false
+
+                                comm = body.trim().split(' ').shift().toLowerCase()
+
+                        }
+
+                        
+
+                        const command = comm
+
+                        
+
+                        const args = body.trim().split(/ +/).slice(1)
+
+                        const isCmd = body.startsWith(prefix)
+
+                        const q = args.join(' ')
+
+                        const soyYo = client.user.jid
+
+                        const botNumber = client.user.jid.split('@')[0]
+
+                        const ownerNumber = ['595994230885']
+
+                        const isGroup = from.endsWith('@g.us')
+
+                        const sender = mek.key.fromMe ? client.user.jid : isGroup ? mek.participant : mek.key.remoteJid
+
+                        const senderNumber = sender.split('@')[0]
+
+                        const conts = mek.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+
+                        const pushname = mek.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
+
+                        
+
+                        const isMe = botNumber.includes(senderNumber)
+
+                        const isOwner = ownerNumber.includes(senderNumber)
+
+                        
+
+                        switch (command) {
+
+                                default:
+
+                                        if (body.startsWith('>')){
+
+                                                if (!q) return
+
+                                                return client.sendMessage(from, JSON.stringify(eval(q), null, 2), text, {quoted: mek})
+
+                                        }
+
+                        }
+
+                } catch (e) {
+
+                        const emror = String(e)
+
+                        console.log(emror)
+
+                }
+
+        })
+
+}
+
+iniciar('./session.json')
